3e11d35274259c9b485369a4675166c4409f7820
ali rios <ali.rios@gmail.com>
2016-09-07 21:18:15 -0300

    Adiciona teste na home1

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -4,11 +4,12 @@ class HomeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/')
 
+
     def test_get(self):
         """ GET / must return status code 200"""
-        self.assertEqual(200,self.response.status_code)
+        self.assertEqual(200, self.response.status_code)
+
 
     def test_template(self):
         """ Must use index.html """
         self.assertTemplateUsed(self.response, 'index.html')
-
