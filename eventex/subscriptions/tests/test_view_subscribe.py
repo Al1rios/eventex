594c288d1bcb80dfd95502e868481e583156b6bf
ali rios <ali.rios@gmail.com>
2016-09-17 21:21:16 -0300

    thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -48,8 +48,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid Post should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid Post should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -82,11 +82,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-#class SubscribeMessage(TestCase):
-#    def test_message(self):
-#        data = dict(name='Ali Rios', cpf='12345678901',
-#                    email='ali.rios@gmail.com', phone='21-97190-6677')
-#        response = self.client.post('/inscricao/', data, follow=True)
-#        self.assertContains(response, 'Inscriçao realizada com sucesso!')
